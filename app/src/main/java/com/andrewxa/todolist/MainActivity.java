ba20b195f43dc775d42760c1c64f089250a3cda1
pelev1n <pelevin9911@gmail.com>
2018-07-10 16:32:36 +0300

    Added TaskAdapter

--- app/src/main/java/com/andrewxa/todolist/MainActivity.java
+++ app/src/main/java/com/andrewxa/todolist/MainActivity.java
@@ -3,27 +3,23 @@ package com.andrewxa.todolist;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
-import android.widget.Toast;
+
+import com.andrewxa.todolist.model.Task;
 
 import java.util.ArrayList;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener, AdapterView.OnItemClickListener {
+public class MainActivity extends AppCompatActivity {
 
     private EditText itemET;
     private Button btn;
-    private ListView itemsList;
+    ArrayList<Task> tasks = new ArrayList();
 
-    private ArrayList<String> items;
 
-    private ArrayAdapter<String> adapter;
+    ListView taskList;
 
 
     @Override
@@ -33,43 +29,23 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         itemET = findViewById(R.id.item_edit_text);
         btn = findViewById(R.id.add_btn);
-        itemsList = findViewById(R.id.items_list);
-        items = FileHelper.readData(this);
 
-        adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, items);
-        itemsList.setAdapter(adapter);
-
-        // Create button handler. After creation
-        btn.setOnClickListener(this);
-        // After creation we'll have to override method onClick
-        // that basically will be our handler.
-
-        itemsList.setOnItemClickListener(this);
-
-    }
-
-    // Button handler.
-    @Override
-    public void onClick(View v) {
-        switch(v.getId()){
-            case R.id.add_btn:
-                String itemEntered = itemET.getText().toString();
-                adapter.add(itemEntered);
-                itemET.setText("");
-                FileHelper.writeData(items, this);
-                Toast.makeText(this, "Item Added", Toast.LENGTH_SHORT).show();
-                break;
-        }
-    }
+        taskList = findViewById(R.id.items_list);
+        final TaskAdapter adapter = new TaskAdapter(this, R.layout.list_item, tasks);
+        taskList.setAdapter(adapter);
 
 
-    @Override
-    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-        items.remove(position);
-        adapter.notifyDataSetChanged();
-        FileHelper.writeData(items, this);
-        Toast.makeText(this, "Delete", Toast.LENGTH_SHORT).show();
-
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                switch (v.getId()) {
+                    case R.id.add_btn:
+                        String itemEntered = itemET.getText().toString();
+                        adapter.add(new Task(itemEntered));
+                        itemET.setText("");
+                        break;
+                }
+            }
+        });
     }
 }
-
