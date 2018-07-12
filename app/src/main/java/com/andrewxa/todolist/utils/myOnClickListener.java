9cbff66d2d6901a52caaf73538fe6d5df9a87526
CaramelHeaven <srg.fominov@gmail.com>
2018-07-12 20:18:04 +0400

    it's not a bad idea, maybe

---
+++ app/src/main/java/com/andrewxa/todolist/utils/myOnClickListener.java
@@ -0,0 +1,9 @@
+package com.andrewxa.todolist.utils;
+
+import android.view.View;
+
+public interface myOnClickListener {
+    void onItemClick(View view, int position, String passString);
+
+    void onSimpleClick(int position);
+}
